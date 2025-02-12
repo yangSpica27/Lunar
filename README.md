b4ec736299b71c3de83567eba1f9d76ab5ae7be9
SPICa27 <spica27@qq.com>
2025-02-12 11:14:06 +0800

    ## update

---
+++ README.md
@@ -0,0 +1,13 @@
+
+## 暂时不知道叫什么才好
+
+>> 突发奇想写的一个查看当天阴历信息的小工具
+
+效果图如下
+
+<div style="display: flex;justify-content: flex-start">
+
+<img src="pic/Screenshot_20250212_111218.png" width="20%"/>
+<img src="pic/Screenshot_20250212_111239.png" width="20%"/>
+
+</div>
--- app/src/main/java/me/spica/lunar/MainActivity.kt
+++ app/src/main/java/me/spica/lunar/MainActivity.kt
@@ -2,40 +2,49 @@ package me.spica.lunar
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.slideIn
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowColumn
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.BlurredEdgeTreatment
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.drawText
@@ -44,20 +53,24 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
 import com.nlf.calendar.Lunar
 import kotlinx.coroutines.delay
 import me.spica.lunar.ui.theme.LunarTheme
+import me.spica.lunar.ui.theme.Text333
+import me.spica.lunar.ui.theme.Text666
 import java.util.Calendar
-import kotlin.time.Duration
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightStatusBars =
+            true
         setContent {
             LunarTheme {
                 Main()
@@ -72,20 +85,50 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun Main() {
+    // 阴历信息
     val lunar = remember { mutableStateOf(Lunar.fromDate(Calendar.getInstance().time)) }
 
+    // 是否展示额外信息面板
+    val showExtraPanel = remember { mutableStateOf(false) }
 
+    // 一分钟左右更新面板信息
     LaunchedEffect(lunar.value) {
         delay(1000 * 60)
         lunar.value = Lunar.fromDate(Calendar.getInstance().time)
     }
 
+    // 上层模糊
+    val backgroundBlurRadius = animateDpAsState(
+        targetValue = if (showExtraPanel.value) 18.dp else 0.dp,
+        animationSpec = tween(
+            durationMillis = 450
+        )
+    )
+
+    // 左边文字（月）的测量器
     val monthTextMeasurer = rememberTextMeasurer()
 
+    // 右边文字（日）的测量器
     val dayTextMeasurer = rememberTextMeasurer()
+
+    // 返回键关闭面板
+    BackHandler(
+        showExtraPanel.value
+    ) {
+        showExtraPanel.value = false
+    }
+
+    // 面板透明度
+    val panelAlpha = animateFloatAsState(
+        if (showExtraPanel.value) 1f else 0f,
+        animationSpec = tween(350, 150)
+    )
 
     Background {
         Scaffold(
+            modifier = Modifier.clickable {
+                showExtraPanel.value = true
+            },
             containerColor = Color.Transparent,
             contentColor = Color.Transparent
         ) { paddingValues ->
@@ -98,6 +141,10 @@ fun Main() {
                     modifier = Modifier
                         .fillMaxSize()
                         .matchParentSize()
+                        .blur(
+                            backgroundBlurRadius.value,
+                            edgeTreatment = BlurredEdgeTreatment.Unbounded
+                        )
                         .padding(horizontal = 24.dp),
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
@@ -115,7 +162,7 @@ fun Main() {
                                 Text(
                                     lunar.value.hou, style = TextStyle(
                                         fontFamily = FontFamily(Font(R.font.lxgw)),
-                                        fontSize = 48.sp,
+                                        fontSize = 40.sp,
                                         color = Color.White
                                     )
                                 )
@@ -146,7 +193,7 @@ fun Main() {
                                     .drawWithCache {
 
                                         val month = (Calendar.getInstance()
-                                            .get(Calendar.DAY_OF_MONTH) + 1).toString()
+                                            .get(Calendar.DAY_OF_MONTH)).toString()
                                         val measureRes =
                                             dayTextMeasurer.measure(
                                                 month,
@@ -196,12 +243,34 @@ fun Main() {
                             FlowColumn(
                                 modifier = Modifier.weight(1f),
                                 horizontalArrangement = Arrangement.spacedBy(10.dp),
-                                verticalArrangement = Arrangement.spacedBy(4.dp),
+                                verticalArrangement = Arrangement.Center,
                             ) {
-                                ("玻璃晴朗，橘子辉煌。").toList()
-                                    .map {
-                                        Text("$it", fontSize = 40.sp, fontWeight = FontWeight.W500)
-                                    }
+                                // 获取当前的节日
+                                val festivals = lunar.value.festivals
+                                //有节日
+                                if (festivals.isNotEmpty()) {
+                                    (lunar.value.festivals).toList().joinToString(",")
+                                        .map {
+                                            Text(
+                                                "$it",
+                                                fontSize = 40.sp,
+                                                fontWeight = FontWeight.W500,
+                                                lineHeight = 3.sp
+                                            )
+                                        }
+                                } else {
+                                    ("今日无事发生。")
+                                        .map {
+                                            Text(
+                                                "$it",
+                                                fontSize = 40.sp,
+                                                fontWeight = FontWeight.W500,
+                                                lineHeight = 3.sp
+                                            )
+                                        }
+                                }
+
+
                             }
                             Spacer(modifier = Modifier.height(12.dp))
                             Text(
@@ -221,11 +290,156 @@ fun Main() {
                             .height(100.dp)
                     ) {
 
+                        // 暂时没想好放什么
+
                     }
 
                 }
+                if (panelAlpha.value != 0f) {
+                    ExtraPanel(
+                        lunar = lunar.value,
+                        modifier = Modifier
+                            .clickable {
+                                showExtraPanel.value = false
+                            }
+                            .fillMaxSize()
+                            .alpha(panelAlpha.value)
+                    )
+                }
             }
         }
+    }
+}
+
+// 更多信息面板
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun ExtraPanel(modifier: Modifier = Modifier, lunar: Lunar) {
+    val show = remember { mutableStateOf(false) }
+
+    SideEffect {
+        show.value = true
+    }
+
+    Box(
+        modifier = modifier.padding(
+            vertical = 40.dp,
+            horizontal = 24.dp
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Text(
+                "今日宜",
+                style = TextStyle(fontSize = 20.sp, color = Text333, letterSpacing = 2.sp)
+            )
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(8.dp)
+            )
+            FlowRow(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                lunar.dayYi.mapIndexed { index, str ->
+
+                    AnimatedVisibility(
+                        show.value,
+                        enter = fadeIn(
+                            animationSpec = tween(
+                                durationMillis = 400 + index * 50,
+                                delayMillis = index * 150
+                            )
+                        ) + slideIn(
+                            animationSpec = tween(
+                                durationMillis = 400 + index * 50,
+                                delayMillis = index * 150
+                            )
+                        ) { _ ->
+                            IntOffset(0, 40)
+                        }
+                    ) {
+                        Text(str, style = TextStyle(fontSize = 18.sp, color = Text666))
+                    }
+                }
+            }
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(20.dp)
+            )
+            Text(
+                "今日忌",
+                style = TextStyle(fontSize = 20.sp, color = Text333, letterSpacing = 2.sp)
+            )
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(8.dp)
+            )
+            FlowRow(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                lunar.dayJi.mapIndexed { index, str ->
+                    AnimatedVisibility(
+                        show.value,
+                        enter = fadeIn(
+                            animationSpec = tween(
+                                durationMillis = 400 + index * 50,
+                                delayMillis = index * 150
+                            )
+                        ) + slideIn(
+                            animationSpec = tween(
+                                durationMillis = 400 + index * 50,
+                                delayMillis = index * 150
+                            )
+                        ) { size ->
+                            IntOffset(0, size.height)
+                        }
+                    ) {
+                        Text(str, style = TextStyle(fontSize = 18.sp, color = Text666))
+                    }
+                }
+            }
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(20.dp)
+            )
+            FlowRow(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalArrangement = Arrangement.Start
+            ) {
+                lunar.solar.toFullString().toList().mapIndexed { index, str ->
+                    AnimatedVisibility(
+                        show.value,
+                        enter = fadeIn(
+                            animationSpec = tween(
+                                durationMillis = 400 + index * 50,
+                                delayMillis = index * 150
+                            )
+                        ) + slideIn(
+                            animationSpec = tween(
+                                durationMillis = 400 + index * 50,
+                                delayMillis = index * 150
+                            )
+                        ) { size ->
+                            IntOffset(0, size.height)
+                        }
+                    ) {
+                        Text(str.toString(), style = TextStyle(fontSize = 18.sp, color = Text333))
+                    }
+                }
+            }
+        }
+
     }
 }
 
